076c71a1ed92ab52287f1eb4ad2f2cd3fc7b8a7a
bormotovilya-ops <bormotovilya@gmail.com>
2025-11-30 01:30:38 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ tg.ready();
 tg.expand();
 
 // 🚨 ВАШ URL СТАНДАРТНОГО ВХОДЯЩЕГО WEBHOOK ИЗ LEADTEH
-const WEBHOOK_BASE_URL = 'https://rb229169.leadteh.ru/inner_webhook/js/19846c85-8252-419d-942c-7e4dc8151977'; 
+const WEBHOOK_BASE_URL = 'https://rb229169.leadteh.ru/inner_webhook/8d3ed841-0230-40a6-b7bc-2edd55cc451b'; 
 
 // 1. Устанавливаем Главную кнопку Telegram для закрытия
 tg.MainButton.setText('Закрыть Mini App').show();
